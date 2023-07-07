1232d2a80959ea644a5365ae9b2e36396e58a4de
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-07-07 13:54:16 +0200

    maybe fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import { D1Database } from "@cloudflare/workers-types";
 export const runtime = "edge";
 
 export default async function Home() {
-  const db = binding<D1Database>("DB");
+  // const db = binding<D1Database>("DB");
   // console.log(db.exec("SELECT * FROM notes;"));
-  const { results } = await db.prepare("SELECT * FROM Customers;").all();
+  // const { results } = await db.prepare("SELECT * FROM Customers;").all();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {JSON.stringify(results)}
+          {/* {JSON.stringify(results)} */}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
