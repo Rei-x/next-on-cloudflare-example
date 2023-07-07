13bcdbadfaf06d41680034d7000c438853c48c68
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-07-07 13:28:30 +0200

    trigger deploy

---
+++ migrations/0000_initial.sql
@@ -0,0 +1,8 @@
+-- Migration number: 0000 	 2023-07-07T11:02:32.133Z
+
+-- sqlite 
+-- create table NOTES
+
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE IF NOT EXISTS Customers (CustomerId INTEGER PRIMARY KEY, CompanyName TEXT, ContactName TEXT);
+INSERT INTO Customers (CustomerID, CompanyName, ContactName) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders'), (4, 'Around the Horn', 'Thomas Hardy'), (11, 'Bs Beverages', 'Victoria Ashworth'), (13, 'Bs Beverages', 'Random Name');
