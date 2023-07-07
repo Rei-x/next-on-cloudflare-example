92919df022b4c5c5263446616b4a441b48beac99
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-07-07 13:33:25 +0200

    trigger deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default async function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            Docssss{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
